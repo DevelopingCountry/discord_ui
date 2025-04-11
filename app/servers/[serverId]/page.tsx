9471c292bb9232488e823265b7b708110e13e2e3
sungjoon <dkdldrha@naver.com>
2025-04-11 11:27:59 +0900

    /servers/[serverId] page,layout

---
+++ app/servers/[serverId]/page.tsx
@@ -0,0 +1,20 @@
+import SectionOne from "@/public/homeDir/ui/sectionOne";
+import SectionFour from "@/public/homeDir/ui/sectionFour";
+import MessageInput from "@/components/messeage-input";
+import SectionOneAndFour from "@/public/homeDir/ui/sectionOneAndFour";
+
+export default function Home() {
+  return (
+    <SectionOneAndFour>
+      <SectionOne>
+        <div className={"text-white"}>welcomePage</div>
+      </SectionOne>
+      <SectionFour>
+        <div className={"flex items-center justify-center text-white"}>welcomePage</div>
+        <div className={"absolute bottom-0 z-20 w-full bg-discord1and4"}>
+          <MessageInput />
+        </div>
+      </SectionFour>
+    </SectionOneAndFour>
+  );
+}
