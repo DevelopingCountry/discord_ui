68c09f5922dc9ad013e5ef16741714726cd33684
sungjoon <dkdldrha@naver.com>
2025-05-13 14:15:38 +0900

    채팅 부분 css 변경

--- public/homeDir/ui/sectionFour.tsx
+++ public/homeDir/ui/sectionFour.tsx
@@ -1,3 +1,3 @@
 export default function SectionFour({ children }: { children: React.ReactNode }) {
-  return <div className={"flex-1 flex bg-discord1and4"}>{children}</div>;
+  return <div className="flex flex-col h-screen bg-discord1and4">{children}</div>;
 }
