866cbedcf67167112d4f1aa87f8f93d1901481a0
sungjoon <dkdldrha@naver.com>
2025-05-14 16:44:19 +0900

    - 서로 친구(모두)일 때 dm거는 버튼(MessageCircle) 활성화

--- components/chat-message.tsx
+++ components/chat-message.tsx
@@ -14,6 +14,7 @@ export default function ChatMessage({
   id,
   isOnline = false,
   isPlaying = false,
+  isActive,
 }: {
   name: string;
   status: string;
@@ -21,6 +22,7 @@ export default function ChatMessage({
   avatar?: string | null;
   isOnline?: boolean;
   isPlaying?: boolean;
+  isActive: string;
 }) {
   const [dmId, setDmId] = useState<string | null>(null);
   const { dmList, addDm } = useDmStore();
@@ -88,38 +90,13 @@ export default function ChatMessage({
         <div className="text-[#b5bac1] text-sm">{status}</div>
       </div>
       <div className="flex space-x-2">
-        <button className="w-9 h-9 rounded-full bg-[#2b2d31] flex items-center justify-center" onClick={clickHandle}>
-          <MessageCircle className="w-5 h-5 text-[#b5bac1]" />
-        </button>
-        <button className="w-9 h-9 rounded-full bg-[#2b2d31] flex items-center justify-center">
-          <svg width="20" height="20" viewBox="0 0 24 24" fill="none">
-            <path fill="currentColor" d="M12 14a2 2 0 1 0 0-4 2 2 0 0 0 0 4z" className="text-[#b5bac1]"></path>
-            <path fill="currentColor" d="M4 8a2 2 0 1 0 0-4 2 2 0 0 0 0 4z" className="text-[#b5bac1]"></path>
-            <path fill="currentColor" d="M4 20a2 2 0 1 0 0-4 2 2 0 0 0 0 4z" className="text-[#b5bac1]"></path>
-            <path fill="currentColor" d="M20 20a2 2 0 1 0 0-4 2 2 0 0 0 0 4z" className="text-[#b5bac1]"></path>
-            <path fill="currentColor" d="M20 8a2 2 0 1 0 0-4 2 2 0 0 0 0 4z" className="text-[#b5bac1]"></path>
-            <path
-              fill="currentColor"
-              d="M12 16.5c0 1.38 1.12 2.5 2.5 2.5h3c1.38 0 2.5-1.12 2.5-2.5v-5c0-1.38-1.12-2.5-2.5-2.5h-3c-1.38 0-2.5 1.12-2.5 2.5v5z"
-              className="text-[#b5bac1]"
-            ></path>
-            <path
-              fill="currentColor"
-              d="M4 16.5c0 1.38 1.12 2.5 2.5 2.5h3c1.38 0 2.5-1.12 2.5-2.5v-5c0-1.38-1.12-2.5-2.5-2.5h-3C5.12 9 4 10.12 4 11.5v5z"
-              className="text-[#b5bac1]"
-            ></path>
-            <path
-              fill="currentColor"
-              d="M4 4.5C4 5.88 5.12 7 6.5 7h3C10.88 7 12 5.88 12 4.5v-1C12 2.12 10.88 1 9.5 1h-3C5.12 1 4 2.12 4 3.5v1z"
-              className="text-[#b5bac1]"
-            ></path>
-            <path
-              fill="currentColor"
-              d="M12 4.5C12 5.88 13.12 7 14.5 7h3C18.88 7 20 5.88 20 4.5v-1C20 2.12 18.88 1 17.5 1h-3C13.12 1 12 2.12 12 3.5v1z"
-              className="text-[#b5bac1]"
-            ></path>
-          </svg>
-        </button>
+        {isActive === "모두" ? (
+          <button className="w-9 h-9 rounded-full bg-[#2b2d31] flex items-center justify-center" onClick={clickHandle}>
+            <MessageCircle className="w-5 h-5 text-[#b5bac1]" />
+          </button>
+        ) : (
+          ""
+        )}
       </div>
     </div>
   );
